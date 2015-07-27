84bf4d4f0898bc999db4239ff856676e0948089f
Matt Lewandowski <mtlewandowsk@gmail.com>
2015-07-27 15:41:53 -0700

    added model

---
+++ src/models/shoe.js
@@ -0,0 +1,22 @@
+var Backbone = require('backbone');
+
+
+/****************************************
+  App
+*****************************************/
+
+var App = require('../app');
+
+/****************************************
+  Model: User
+*****************************************/
+
+App.Models.Shoe = Backbone.Model.extend({
+  url: function() {
+    var base = App.Settings.apiRoot + '/shoe';
+    if (this.isNew()) return base;
+    return base + '/' + this.id
+  }
+});
+
+module.exports = App.Models.Shoe;
